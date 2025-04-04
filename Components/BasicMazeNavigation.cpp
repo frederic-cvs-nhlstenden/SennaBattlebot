73cc3a71b36834df8cc81c17820d3cf20914557c
Frederic Cahn von Seelen <entire.twice-0v@icloud.com>
2025-04-04 14:38:43 +0200

    Game Logic fully completed, committing before changing starting and ending sequence

--- Components/BasicMazeNavigation.cpp
+++ Components/BasicMazeNavigation.cpp
@@ -1,13 +1,13 @@
 #include <Arduino.h>
 
 // ----------------- ARDUINO CONSTANTS -----------------
-const int MOTOR_LEFT_FWD  = 10; // A2: left motor forward
-const int MOTOR_LEFT_REV  = 11; // A1: left motor reverse
-const int MOTOR_RIGHT_FWD = 6;  // B1: right motor forward
-const int MOTOR_RIGHT_REV = 5;  // B2: right motor reverse
+const int MOTOR_LEFT_FWD = 10; // A2: left motor forward
+const int MOTOR_LEFT_REV = 11; // A1: left motor reverse
+const int MOTOR_RIGHT_FWD = 6; // B1: right motor forward
+const int MOTOR_RIGHT_REV = 5; // B2: right motor reverse
 
-const int TRIG_PIN_RIGHT  = 4;  // right ultrasonic trig
-const int ECHO_PIN_RIGHT  = 8;  // right ultrasonic echo
+const int TRIG_PIN_RIGHT = 4; // right ultrasonic trig
+const int ECHO_PIN_RIGHT = 8; // right ultrasonic echo
 
 // Target distance from right wall in cm
 const int RIGHT_TARGET = 5;
@@ -26,66 +26,87 @@ const int MIN_PWM = 100;
 bool flipPidSign = true;
 
 // Rotation sensor pins
-const int LEFT_SENSOR_PIN  = 3; // Left Wheel
+const int LEFT_SENSOR_PIN = 3;  // Left Wheel
 const int RIGHT_SENSOR_PIN = 2; // Right Wheel
+
+// Gripper Parameters
+const int GRIP_PIN = 7;
+const int OPEN_GRIP = 1600; // 1600
+const int CLOSE_GRIP = 990; // 990
+bool pickedUpCone = false;
+bool droppedOffCone = false;
+
+const int SENSOR_PINS[] = {A0, A1, A2, A3, A4, A5, A6, A7};
+int SENSOR_VALUES[8];
+int SENSOR_WEIGHTS[] = {-3500, -2500, -1500, -500, 500, 1500, 2500, 3500};
+
+int sensorMin[8];
+int sensorMax[8];
+int sensorThresholds[8];
+
+bool LINE_SENSOR_IS_CALIBRATED = false;
 
 // ----- TURN PARAMETERS -----
 // Wheel and Rotation Sensor Parameters
-const float WHEEL_DIAMETER      = 6.0; // cm
+const float WHEEL_DIAMETER = 6.0; // cm
 const float WHEEL_CIRCUMFERENCE = 3.14 * WHEEL_DIAMETER;
-const int   PULSES_PER_REV      = 20;
-const float DIST_PER_PULSE      = WHEEL_CIRCUMFERENCE / PULSES_PER_REV;
+const int PULSES_PER_REV = 20;
+const float DIST_PER_PULSE = WHEEL_CIRCUMFERENCE / PULSES_PER_REV;
 const float TRACK_WIDTH = 9.3; // cm
-const int LEFT_TARGET_SPEED  = 255;
+const int LEFT_TARGET_SPEED = 255;
 const int RIGHT_TARGET_SPEED = 240;
 
 // Motor ramp up speeds for L&R
-const int leftRampSpeeds[4]  = {
-  LEFT_TARGET_SPEED / 4,
-  LEFT_TARGET_SPEED / 2,
-  (LEFT_TARGET_SPEED * 3) / 4,
-  LEFT_TARGET_SPEED
-};
+const int leftRampSpeeds[4] = {
+    LEFT_TARGET_SPEED / 4,
+    LEFT_TARGET_SPEED / 2,
+    (LEFT_TARGET_SPEED * 3) / 4,
+    LEFT_TARGET_SPEED};
 
 const int rightRampSpeeds[4] = {
-  RIGHT_TARGET_SPEED / 4,
-  RIGHT_TARGET_SPEED / 2,
-  (RIGHT_TARGET_SPEED * 3) / 4,
-  RIGHT_TARGET_SPEED
-};
+    RIGHT_TARGET_SPEED / 4,
+    RIGHT_TARGET_SPEED / 2,
+    (RIGHT_TARGET_SPEED * 3) / 4,
+    RIGHT_TARGET_SPEED};
 
 const unsigned long RAMP_DELAY = 250;
 
 // Global Variables for ISR
-volatile unsigned long pulseCountLeft  = 0; 
+volatile unsigned long pulseCountLeft = 0;
 volatile unsigned long pulseCountRight = 0;
-volatile bool lastStateLeft  = LOW;
+volatile bool lastStateLeft = LOW;
 volatile bool lastStateRight = LOW;
 
 volatile bool motorsActive = false;
-volatile bool testActive   = true;
+volatile bool testActive = true;
 
 // Pins for forward ultrasound
-const int TRIG_PIN          = 13;
-const int ECHO_PIN          = 12;
+const int TRIG_PIN = 13;
+const int ECHO_PIN = 12;
 const int OBSTACLE_THRESHOLD = 10; // cm
 
 // ISRs
-void pulseLeft() {
-  if (!motorsActive) return;
+void pulseLeft()
+{
+  if (!motorsActive)
+    return;
 
   bool currentState = digitalRead(LEFT_SENSOR_PIN);
-  if (currentState != lastStateLeft) {
+  if (currentState != lastStateLeft)
+  {
     pulseCountLeft++;
     lastStateLeft = currentState;
   }
 }
 
-void pulseRight() {
-  if (!motorsActive) return;
+void pulseRight()
+{
+  if (!motorsActive)
+    return;
 
   bool currentState = digitalRead(RIGHT_SENSOR_PIN);
-  if (currentState != lastStateRight) {
+  if (currentState != lastStateRight)
+  {
     pulseCountRight++;
     lastStateRight = currentState;
   }
@@ -94,7 +115,8 @@ void pulseRight() {
 // ---------------------------------------------------
 
 // Measuring distance via ultrasonic
-long measureDistanceRight() {
+long measureDistanceRight()
+{
   digitalWrite(TRIG_PIN_RIGHT, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIG_PIN_RIGHT, HIGH);
@@ -102,7 +124,8 @@ long measureDistanceRight() {
   digitalWrite(TRIG_PIN_RIGHT, LOW);
 
   long duration = pulseIn(ECHO_PIN_RIGHT, HIGH, 30000); //
-  if (duration == 0) {
+  if (duration == 0)
+  {
     // No echo => out of range
     return -1;
   }
@@ -111,18 +134,23 @@ long measureDistanceRight() {
 }
 
 // Clamping speeds so we don't stall and we don't get any unexpected behavior
-int clampPWM(int val) {
-  if (val < 0)   val = 0; 
-  if (val > 255) val = 255;
-  if (val > 0 && val < MIN_PWM) {
+int clampPWM(int val)
+{
+  if (val < 0)
+    val = 0;
+  if (val > 255)
+    val = 255;
+  if (val > 0 && val < MIN_PWM)
+  {
     val = MIN_PWM;
   }
   return val;
 }
 
 // Set motors to drive forward with given clamped speeds
-void setMotorSpeedsForward(int leftSpeed, int rightSpeed) {
-  leftSpeed  = clampPWM(leftSpeed);
+void setMotorSpeedsForward(int leftSpeed, int rightSpeed)
+{
+  leftSpeed = clampPWM(leftSpeed);
   rightSpeed = clampPWM(rightSpeed);
 
   // Left motor forward
@@ -140,7 +168,8 @@ void setMotorSpeedsForward(int leftSpeed, int rightSpeed) {
   Serial.println(rightSpeed);
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 
   pinMode(MOTOR_LEFT_FWD, OUTPUT);
@@ -159,15 +188,18 @@ void setup() {
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
 
+  pinMode(GRIP_PIN, OUTPUT);
+  digitalWrite(GRIP_PIN, LOW);
+
   // Wheel rotation sensors
   pinMode(LEFT_SENSOR_PIN, INPUT_PULLUP);
   pinMode(RIGHT_SENSOR_PIN, INPUT_PULLUP);
 
   // Initialize last rotation sensor states to the current reads
-  lastStateLeft  = digitalRead(LEFT_SENSOR_PIN);
+  lastStateLeft = digitalRead(LEFT_SENSOR_PIN);
   lastStateRight = digitalRead(RIGHT_SENSOR_PIN);
 
-  attachInterrupt(digitalPinToInterrupt(LEFT_SENSOR_PIN),  pulseLeft,  CHANGE);
+  attachInterrupt(digitalPinToInterrupt(LEFT_SENSOR_PIN), pulseLeft, CHANGE);
   attachInterrupt(digitalPinToInterrupt(RIGHT_SENSOR_PIN), pulseRight, CHANGE);
 
   Serial.println("=== Minimal Right-Wall-Hugging PID ===");
@@ -177,7 +209,8 @@ void setup() {
   delay(10);
 }
 
-void stopMotors() {
+void stopMotors()
+{
   analogWrite(MOTOR_LEFT_REV, 0);
   analogWrite(MOTOR_LEFT_FWD, 0);
   analogWrite(MOTOR_RIGHT_FWD, 0);
@@ -186,9 +219,10 @@ void stopMotors() {
 }
 
 // Calculating distance travelled on the basis of the rotary encoders
-float getDistanceTraveled() {
+float getDistanceTraveled()
+{
   noInterrupts();
-  unsigned long leftCount  = pulseCountLeft;
+  unsigned long leftCount = pulseCountLeft;
   unsigned long rightCount = pulseCountRight;
   interrupts();
 
@@ -197,7 +231,8 @@ float getDistanceTraveled() {
 }
 
 // Measuring distance with front mounted ultrasound
-long measureDistance() {
+long measureDistance()
+{
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIG_PIN, HIGH);
@@ -209,35 +244,36 @@ long measureDistance() {
   return distance;
 }
 
-void turnDegrees(int degrees) {
-  // Resetting pulses and last states from ISR
+void turnDegrees(int degrees)
+{
+  // Reseting encoder and pulse states to mitigate any anomalous readings
   noInterrupts();
-  pulseCountLeft  = 0;
+  pulseCountLeft = 0;
   pulseCountRight = 0;
-  lastStateLeft   = digitalRead(LEFT_SENSOR_PIN);
-  lastStateRight  = digitalRead(RIGHT_SENSOR_PIN);
+  lastStateLeft = digitalRead(LEFT_SENSOR_PIN);
+  lastStateRight = digitalRead(RIGHT_SENSOR_PIN);
   interrupts();
 
-  // Arc distance calculation and required pulses accordingly
-  float arcDistance    = (3.14 * TRACK_WIDTH * abs(degrees)) / 360.0;
+  // Calculating the distance and respective pulses required
+  float arcDistance = (3.14 * TRACK_WIDTH * abs(degrees)) / 360.0;
   float requiredPulses = arcDistance / DIST_PER_PULSE;
 
   motorsActive = true;
-
-  // highTurnSpeed is used for the initial phase and lowTurnSpeed for the end phase.
-  const int highTurnSpeed = LEFT_TARGET_SPEED;
-  const int lowTurnSpeed  = 150;
-
-  // Threshold for switching to low speed to mitigate slip at the end of the turn (avoids overrotation)
+  const int highTurnSpeed = LEFT_TARGET_SPEED; // initial higher speed
+  const int lowTurnSpeed = 150;                // slow near the end
   float speedSwitchThreshold = requiredPulses * 0.8;
 
-  if (degrees > 0) {
+  // Setting motors up depending on turn direction
+  if (degrees > 0)
+  {
     // Turn left
     analogWrite(MOTOR_LEFT_REV, highTurnSpeed);
     analogWrite(MOTOR_LEFT_FWD, 0);
     analogWrite(MOTOR_RIGHT_FWD, highTurnSpeed);
     analogWrite(MOTOR_RIGHT_REV, 0);
-  } else {
+  }
+  else
+  {
     // Turn right
     analogWrite(MOTOR_LEFT_FWD, highTurnSpeed);
     analogWrite(MOTOR_LEFT_REV, 0);
@@ -245,58 +281,242 @@ void turnDegrees(int degrees) {
     analogWrite(MOTOR_RIGHT_FWD, 0);
   }
 
-  // Continue turning while pulse counts
-  while (((pulseCountLeft + pulseCountRight) / 2.0) < requiredPulses) {
+  // Time limit check; if turn not complete in 3 seconds, escape
+  unsigned long startTime = millis();
+  while (((pulseCountLeft + pulseCountRight) / 2.0) < requiredPulses)
+  {
+    // Esacpe squence after 3 seconds
+    if (millis() - startTime >= 3000UL)
+    {
+      Serial.println("Turn timed out (3s). Reversing to free ourselves.");
+      stopMotors();
+
+      analogWrite(MOTOR_LEFT_REV, clampPWM(200));
+      analogWrite(MOTOR_LEFT_FWD, 0);
+      analogWrite(MOTOR_RIGHT_REV, clampPWM(200));
+      analogWrite(MOTOR_RIGHT_FWD, 0);
+      delay(1000);
+      stopMotors();
+      delay(1000);
+      turnDegrees(90);
+      delay(100);
+
+      stopMotors();
+      motorsActive = false;
+      return; // Returning early to go back to main loop
+    }
+
+    // Switching to lower speed near the end to avoid overshoot
     float avgPulses = (pulseCountLeft + pulseCountRight) / 2.0;
-    // When we reach the threshold, reduce to lower speed
-    if (avgPulses >= speedSwitchThreshold) {
-      if (degrees > 0) {
+    if (avgPulses >= speedSwitchThreshold)
+    {
+      if (degrees > 0)
+      {
         analogWrite(MOTOR_LEFT_REV, lowTurnSpeed);
         analogWrite(MOTOR_RIGHT_FWD, lowTurnSpeed);
-      } else {
+      }
+      else
+      {
         analogWrite(MOTOR_LEFT_FWD, lowTurnSpeed);
         analogWrite(MOTOR_RIGHT_REV, lowTurnSpeed);
       }
     }
+
     delay(10);
   }
 
+  // If the turn is finished on time, stop and do a quick braking pulse to counteract inertia
   stopMotors();
-
-  if (degrees > 0) {
+  if (degrees > 0)
+  {
     analogWrite(MOTOR_LEFT_FWD, 100);  // left forward
-    analogWrite(MOTOR_LEFT_REV, 0);
-    analogWrite(MOTOR_RIGHT_REV, 100);  // right reverse
-    analogWrite(MOTOR_RIGHT_FWD, 0);
-  } else {
+    analogWrite(MOTOR_RIGHT_REV, 100); // right reverse
+  }
+  else
+  {
     analogWrite(MOTOR_LEFT_REV, 100);  // left reverse
-    analogWrite(MOTOR_LEFT_FWD, 0);
-    analogWrite(MOTOR_RIGHT_FWD, 100);  // right forward
-    analogWrite(MOTOR_RIGHT_REV, 0);
+    analogWrite(MOTOR_RIGHT_FWD, 100); // right forward
   }
   delay(50);
+
   stopMotors();
+  motorsActive = false;
 }
 
-void checkLeft() {
-    if (measureDistance() < OBSTACLE_THRESHOLD) {
-        stopMotors();
-        delay(1000);
-        turnDegrees(90);
-        delay(1000);
+void checkLeft()
+{
+  if (measureDistance() < OBSTACLE_THRESHOLD)
+  {
+    stopMotors();
+    delay(1000);
+    turnDegrees(90);
+    delay(1000);
+  }
+  return;
+}
+
+void gripper(int pulse)
+{
+  static unsigned long timer;
+  static int lastPulse;
+
+  if (pulse > 0)
+  {
+    lastPulse = pulse; // Store the last requested pulse width
+  }
+
+  if (millis() >= timer)
+  { // Every 20ms (50Hz signal)
+    digitalWrite(GRIP_PIN, HIGH);
+    delayMicroseconds(lastPulse); // Keep the gripper holding position
+    digitalWrite(GRIP_PIN, LOW);
+    timer = millis() + 20;
+  }
+}
+
+void calibrateSensors()
+{
+  // Initializing each sensor min and max to their respective values
+  for (int i = 0; i < 8; i++)
+  {
+    sensorMin[i] = 1023;
+    sensorMax[i] = 0;
+  }
+
+  // Speed of motors for calibration
+  const int CALIB_SPEED = 255;
+
+  motorsActive = true; // Letting ISRs count pulses
+
+  // Drive Forward for 1 second
+  analogWrite(MOTOR_LEFT_FWD, CALIB_SPEED);
+  analogWrite(MOTOR_LEFT_REV, 0);
+  analogWrite(MOTOR_RIGHT_FWD, CALIB_SPEED);
+  analogWrite(MOTOR_RIGHT_REV, 0);
+
+  unsigned long startTime = millis();
+  while (millis() - startTime < 1000UL)
+  {
+    // Continuously updating sensorMin / sensorMax
+    for (int i = 0; i < 8; i++)
+    {
+      int sensorValue = analogRead(SENSOR_PINS[i]);
+      if (sensorValue < sensorMin[i])
+        sensorMin[i] = sensorValue;
+      if (sensorValue > sensorMax[i])
+        sensorMax[i] = sensorValue;
     }
-    return;
+    delay(5); // Small delay to reduce noise
+  }
+
+  // Driving backwards to original position
+  analogWrite(MOTOR_LEFT_FWD, 0);
+  analogWrite(MOTOR_LEFT_REV, CALIB_SPEED);
+  analogWrite(MOTOR_RIGHT_FWD, 0);
+  analogWrite(MOTOR_RIGHT_REV, CALIB_SPEED);
+
+  startTime = millis();
+  while (millis() - startTime < 1000UL)
+  {
+    for (int i = 0; i < 8; i++)
+    {
+      int sensorValue = analogRead(SENSOR_PINS[i]);
+      if (sensorValue < sensorMin[i])
+        sensorMin[i] = sensorValue;
+      if (sensorValue > sensorMax[i])
+        sensorMax[i] = sensorValue;
+    }
+    delay(5);
+  }
+
+  stopMotors();
+  motorsActive = false;
+
+  // Thresholds computed as aveg of min and max for each sensor
+  for (int i = 0; i < 8; i++)
+  {
+    sensorThresholds[i] = (sensorMin[i] + sensorMax[i]) / 2;
+  }
+
+  for (int i = 0; i < 8; i++)
+  {
+    Serial.print("Sensor ");
+    Serial.print(i);
+    Serial.print(": min=");
+    Serial.print(sensorMin[i]);
+    Serial.print(", max=");
+    Serial.print(sensorMax[i]);
+    Serial.print(", threshold=");
+    Serial.println(sensorThresholds[i]);
+  }
+
+  LINE_SENSOR_IS_CALIBRATED = true;
 }
 
-void loop() {
+bool isAllBlack()
+{
+  // Read each sensor and compare to its threshold
+  for (int i = 0; i < 8; i++)
+  {
+    int reading = analogRead(SENSOR_PINS[i]);
+    // If the square underneath the sensor is black, the readings exceed the thresholds
+    // otherwise, the readings stay below, and so we return false
+    if (reading <= sensorThresholds[i])
+    {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+void loop()
+{
+
+  /* if (!LINE_SENSOR_IS_CALIBRATED)
+  {
+    calibrateSensors();
+  }
+
+  Serial.print("Sensor readings: ");
+  for (int i = 0; i < 8; i++)
+  {
+    int reading = analogRead(SENSOR_PINS[i]);
+    Serial.print(reading);
+    Serial.print("/");
+    Serial.print(sensorThresholds[i]);
+    Serial.print("  ");
+  }
+  Serial.println();
+
+  if (isAllBlack() && !pickedUpCone)
+  {
+    for (int i = 0; i < 5; i++)
+    {
+      gripper(CLOSE_GRIP);
+    }
+    pickedUpCone = true;
+    turnDegrees(90);
+  }
+  if (isAllBlack() && pickedUpCone && !droppedOffCone)
+  {
+    for (int i = 0; i < 5; i++)
+    {
+      gripper(OPEN_GRIP);
+    }
+    droppedOffCone = true;
+    stopMotors();
+  } */
   checkLeft();
   // Measure right distance
   long distRight = measureDistanceRight();
-  if (distRight < 0) distRight = 999;
+  if (distRight < 0)
+    distRight = 999;
 
   // Error
   float error = distRight - RIGHT_TARGET;
-  if (flipPidSign) {
+  if (flipPidSign)
+  {
     error = -error;
   }
 
@@ -304,7 +524,7 @@ void loop() {
   float correction = Kp_right * error;
 
   // Raw speeds
-  int leftSpeed  = (int)(baseForwardSpeed - correction);
+  int leftSpeed = (int)(baseForwardSpeed - correction);
   int rightSpeed = (int)(baseForwardSpeed + correction);
 
   // --- Debug prints ---
